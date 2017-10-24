22b362fff065d81b92931c481a1b861af83f6a36
Thomas Allerton <thomas.allerton@imperial.ac.uk>
2017-10-24 15:33:38 +0100

    [TA] Changed how viewing existing notes works

--- FrontEnd/app/src/main/java/mapnotes/mapnotes/NoteDisplayActivity.java
+++ FrontEnd/app/src/main/java/mapnotes/mapnotes/NoteDisplayActivity.java
@@ -2,6 +2,9 @@ package mapnotes.mapnotes;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,15 +18,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import mapnotes.mapnotes.data_classes.DateAndTime;
 import mapnotes.mapnotes.data_classes.Note;
 
-public class NoteDisplayActivity extends FragmentActivity implements OnMapReadyCallback {
+public class NoteDisplayActivity extends FragmentActivity {
 
-    private GoogleMap mMap;
     private Note thisNote;
 
 
@@ -32,33 +37,52 @@ public class NoteDisplayActivity extends FragmentActivity implements OnMapReadyC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_display);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        initialise();
     }
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
+     *
+     * Initialise the UI
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
+    public void initialise() {
         //Initial UI settings
-        mMap.getUiSettings().setMapToolbarEnabled(true);
 
         //Try and find location to zoom into and set initial marker
         Intent i = getIntent();
         thisNote = (Note) i.getParcelableExtra("note");
 
         if (thisNote != null) {
-            LatLng marker = thisNote.getLocation();
-            mMap.addMarker(new MarkerOptions().position(marker));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 15));
-            thisNote.setLocation(marker);
-            updateUI();
+            Geocoder geocoder;
+            final LatLng location = thisNote.getLocation();
+            List<Address> addresses;
+            geocoder = new Geocoder(this, Locale.getDefault());
+            try {
+                addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                TextView locationText = findViewById(R.id.location_text);
+                if (addresses != null) {
+                    final Address returnedAddress = addresses.get(0);
+                    locationText.setText(returnedAddress.getAddressLine(0));
+                    locationText.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // Create a Uri from an intent string. Use the result to create an Intent.
+                            Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + returnedAddress.getAddressLine(0));
+
+                            // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+                            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                            // Make the Intent explicit by setting the Google Maps package
+                            mapIntent.setPackage("com.google.android.apps.maps");
+
+                            // Attempt to start an activity that can handle the Intent
+                            startActivity(mapIntent);
+                        }
+                    });
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
             finish();
         }
@@ -87,24 +111,12 @@ public class NoteDisplayActivity extends FragmentActivity implements OnMapReadyC
         updateTimes(startDate, startTime, thisNote.getTime());
         updateTimes(endDate, endTime, thisNote.getEndTime());
 
+
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if (mMap != null) {
-            updateUI();
-        }
-    }
-
-    private void updateUI() {
-        if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-            mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        } else {
-            mMap.setMyLocationEnabled(false);
-            mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        }
     }
 
     private void updateTimes(TextView dateView, TextView timeView, DateAndTime time) {
